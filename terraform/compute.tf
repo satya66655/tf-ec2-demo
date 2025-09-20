602f5eda8d1739b0c714c88bf872d96e1b51bffd
Satya <satya66655@gmail.com>
2025-09-20 07:38:54 +0000

    Changed the Ec2 form T3.micro to T2.micro

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -17,7 +17,7 @@ data "aws_ami" "al2023" {
 
 resource "aws_instance" "this" {
   ami                         = data.aws_ami.al2023.id
-  instance_type               = "t3.micro"
+  instance_type               = "t2.micro"
   subnet_id                   = local.default_subnet_id
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
   key_name                    = var.key_name
